405169762f9dce192c5c0356d87d568dd1425932
Golin <36920999+sztupyrc@users.noreply.github.com>
2021-10-15 12:12:53 +0200

    Added logos

---
+++ armour_bronze.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="armour_bronze" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="src/assets/armour_bronze.png" width="32" height="32"/>
+</tileset>
