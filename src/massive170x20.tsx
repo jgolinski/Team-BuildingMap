e8f70f48e046b9a2ef038b76abc2d71428bd6967
Golin <36920999+sztupyrc@users.noreply.github.com>
2021-10-15 12:50:59 +0200

    PLeaseV2

--- src/massive170x20.tsx
+++ src/massive170x20.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="massive170x20" tilewidth="170" tileheight="20" tilecount="1" columns="1">
- <image source="src/assets/massive170x20.png" width="170" height="20"/>
+ <image source="assets/massive170x20.png" width="170" height="20"/>
 </tileset>
--- src/trace.tsx
+++ src/trace.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.7.2" name="trace" tilewidth="64" tileheight="13" tilecount="1" columns="1">
- <image source="src/assets/trace.png" width="64" height="13"/>
+ <image source="assets/trace.png" width="64" height="13"/>
 </tileset>
